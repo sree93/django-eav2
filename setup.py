cf6ec2cc783e46fb5021fe82515d3117fc75afc8
Sree <sree93@users.noreply.github.com>
2020-09-30 01:49:12 +0530

    setup changed to tb

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'django-eav2-sree',
+    name             = 'travisb-django-eav2',
     version          = __import__('eav').__version__,
     license          = 'GNU Lesser General Public License (LGPL), Version 3',
     requires         = ['python (>= 3.5)', 'django (>= 1.11.14)'],
     provides         = ['eav'],
-    description      = 'Entity-Attribute-Value storage for Django',
+    description      = 'Entity-Attribute-Value storage for Django. Fork of django-eav-2 by Iwo Herka [hi@iwoherka.eu]',
     url              = 'http://github.com/sree93/django-eav2',
     packages         = find_packages(),
-    maintainer       = 'Iwo Herka',
-    maintainer_email = 'hi@iwoherka.eu',
+    maintainer       = 'Srinath Menon',
+    maintainer_email = 'srinath@travisbickle.tech',
 
     classifiers  = [
         'Development Status :: 3 - Alpha',
