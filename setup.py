16e73850ca172f1ec09de014d08d28d8b23474a1
Sree <sree93@users.noreply.github.com>
2020-09-30 01:40:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     requires         = ['python (>= 3.5)', 'django (>= 1.11.14)'],
     provides         = ['eav'],
     description      = 'Entity-Attribute-Value storage for Django',
-    url              = 'http://github.com/makimo/django-eav2',
+    url              = 'http://github.com/sree93/django-eav2',
     packages         = find_packages(),
     maintainer       = 'Iwo Herka',
     maintainer_email = 'hi@iwoherka.eu',
